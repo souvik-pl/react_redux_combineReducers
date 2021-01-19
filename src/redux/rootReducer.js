e2aa1b9f8fc73a26d471295b75a606d34b27fab7
Souvik Paul <souvik.9401@gmail.com>
2021-01-19 13:57:48 +0530

    initial commit

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import TwoReducer from './multiply_two/TwoReducer';
+import FiveReducer from './multiply_five/FiveReducer';
+
+
+const rootReducer = combineReducers({
+	two: TwoReducer,
+	five: FiveReducer
+})
+
+export default rootReducer;
